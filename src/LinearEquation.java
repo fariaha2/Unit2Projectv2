6ee93bc2a5bc069391791f61fa6ed8ab515fa13a
BT_2N2_13 <jgf@gmail.com>
2023-10-16 14:03:50 -0400

    hh

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,8 +1,16 @@
 public class LinearEquation {
+    private int x1;
+    private int x2;
+    private int y1;
+    private int y2;
+
     public LinearEquation(String coord1, String coord2) {
-        int x1 = Integer.parseInt(coord1.substring(1, 2));
-        int x2 = Integer.parseInt(coord2.substring(1, 2));
-        int y1 = Integer.parseInt(coord1.substring(3, 4));
-        int y2 = Integer.parseInt(coord2.substring(3, 4));
+        x1 = Integer.parseInt(coord1.substring(1, 2));
+        x2 = Integer.parseInt(coord2.substring(1, 2));
+        y1 = Integer.parseInt(coord1.substring(3, 4));
+        y2 = Integer.parseInt(coord2.substring(3, 4));
+    }
+    public double distance() {
+        return Math.sqrt((Math.pow((x2-x1), 2)) + (Math.pow((y2-y1), 2)));
     }
 }
